4ec657b1981893bae9e97bc4e2db3455ba98687c
Daniel Sogl <daniel.sogl@thinktecture.com>
2025-05-21 13:41:51 +0200

    feat: add regression and API test tags to Playwright configuration and update relevant test files for better organization and categorization

--- e2e/navigate.spec.ts
+++ e2e/navigate.spec.ts
@@ -1,33 +1,39 @@
 import test, { expect } from '@playwright/test';
 
-test.describe('Übung 1 - Navigation mit Assertions', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  test('Navigiere auf die Public News Seite und prüfe URL', async ({
-    page,
-  }) => {
-    // Element anhand des exakten Textes finden und klicken
-    // await page.getByText('Public News', { exact: true }).click();
-
-    await page
-      .getByRole('menuitem', { name: 'Navigate to Public News' })
-      .click();
-
-    await expect(page).toHaveURL('/news/public');
-  });
-
-  test('Navigiere auf die Public News Seite und prüfe Titel', async ({
-    page,
-  }) => {
-    await page
-      .getByRole('menuitem', { name: 'Navigate to Public News' })
-      .click();
-
-    // Dieser Test wartet 20 Sekunden maximal, da das Backend 10 Sekunden lang läuft
-    await expect(page.getByRole('heading', { name: 'News Feed' })).toBeVisible({
-      timeout: 20000,
+test.describe(
+  'Übung 1 - Navigation mit Assertions',
+  { tag: ['@regression'] },
+  () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/');
     });
-  });
-});
+
+    test('Navigiere auf die Public News Seite und prüfe URL', async ({
+      page,
+    }) => {
+      // Element anhand des exakten Textes finden und klicken
+      // await page.getByText('Public News', { exact: true }).click();
+
+      await page
+        .getByRole('menuitem', { name: 'Navigate to Public News' })
+        .click();
+
+      await expect(page).toHaveURL('/news/public');
+    });
+
+    test('Navigiere auf die Public News Seite und prüfe Titel', async ({
+      page,
+    }) => {
+      await page
+        .getByRole('menuitem', { name: 'Navigate to Public News' })
+        .click();
+
+      // Dieser Test wartet 20 Sekunden maximal, da das Backend 10 Sekunden lang läuft
+      await expect(
+        page.getByRole('heading', { name: 'News Feed' }),
+      ).toBeVisible({
+        timeout: 20000,
+      });
+    });
+  },
+);
